81affa5416f73d5051bea535afc5e845e9902a1e
Song TaeHwan <axoghks@gmail.com>
2022-05-02 20:19:12 +0900

    feat: Show main view controller programmatically and delete Main.storyboard

---
+++ LoginProcess/LoginProcess/Controller/LoginViewController.swift
@@ -0,0 +1,25 @@
+//
+//  LoginViewController.swift
+//  LoginProcess
+//
+//  Created by 송태환 on 2022/05/02.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.configureUI()
+    }
+
+    private func configureUI() {
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.colors = [UIColor.systemPurple.cgColor, UIColor.systemBlue.cgColor]
+        gradientLayer.locations = [0, 1]
+        self.view.layer.addSublayer(gradientLayer)
+        gradientLayer.frame = self.view.bounds
+    }
+
+
+}
