1b80bda4a7b50458d8dda85499326dceb8e1b195
Song TaeHwan <axoghks@gmail.com>
2022-05-03 14:29:39 +0900

    feat: Add common UIColor to extensions

---
+++ LoginProcess/LoginProcess/Utility/UIColor+Extensions.swift
@@ -0,0 +1,14 @@
+//
+//  UIColor+Extensions.swift
+//  LoginProcess
+//
+//  Created by 송태환 on 2022/05/03.
+//
+
+import UIKit
+
+extension UIColor {
+    static let label = UIColor(white: 1, alpha: 0.87)
+    static let placeholder = UIColor(white: 1, alpha: 0.7)
+    static let divider = UIColor(white: 1, alpha: 0.25)
+}
